12b453362024837da549d5a148efd44f62db0b81
Kenneth Leine Schulstad <kenneth@leine.cc>
2016-10-07 23:21:29 +0200

    fix: hide user.accessToken related endpoints

--- common/models/game-master.js
+++ common/models/game-master.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = function(GameMaster) {
-
+  GameMaster.disableRemoteMethod('__count__accessTokens', false);
+  GameMaster.disableRemoteMethod('__create__accessTokens', false);
+  GameMaster.disableRemoteMethod('__delete__accessTokens', false);
+  GameMaster.disableRemoteMethod('__destroyById__accessTokens', false);
+  GameMaster.disableRemoteMethod('__findById__accessTokens', false);
+  GameMaster.disableRemoteMethod('__get__accessTokens', false);
+  GameMaster.disableRemoteMethod('__updateById__accessTokens', false);
 };
--- common/models/player.js
+++ common/models/player.js
@@ -1,4 +1,11 @@
 'use strict';
 
 module.exports = function(Player) {
+  Player.disableRemoteMethod('__count__accessTokens', false);
+  Player.disableRemoteMethod('__create__accessTokens', false);
+  Player.disableRemoteMethod('__delete__accessTokens', false);
+  Player.disableRemoteMethod('__destroyById__accessTokens', false);
+  Player.disableRemoteMethod('__findById__accessTokens', false);
+  Player.disableRemoteMethod('__get__accessTokens', false);
+  Player.disableRemoteMethod('__updateById__accessTokens', false);
 };
